083ba11501f79a5b8a2fb5bd7dc8d7b9c7a70317
Julian <julianmcalhoun@gmail.com>
2020-08-16 23:32:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -11,15 +11,6 @@ function teenager(age)
   if (age >=13 && age <=19)
   {
     return "You are a teenager!"
-    
-    else if (age <=12)
-    {
-      return "You are a kid"
-    }
-    else (age >=20)
-    {
-      return "You are a grownup"
-    }
   }
 }
 
@@ -29,7 +20,14 @@ function ageChecker(age)
   {
     return "You are a teenager!"
   }
-    else return "You are not a teenager"
+    else if (age <=12)
+    {
+      return "You are a kid"
+    }
+    else (age >=20)
+    {
+      return "You are a grownup"
+    }
 }
 
 function ternaryTeenager(age) {
