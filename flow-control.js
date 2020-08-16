51c7c67e1b4529365962f7e21b6cf475db8b36a2
Julian <julianmcalhoun@gmail.com>
2020-08-16 23:42:32 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -34,8 +34,10 @@ function ternaryTeenager(age)
 {
   if (age >= 13 && age <= 19)
   {
-    return "You are not a teenager"
+    return "You are a teenager"
   }
+  
+  else return "You are not a teenager"
 }
 
 function switchAge(age) {
